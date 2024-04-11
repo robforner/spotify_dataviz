d828d267d599745ddab6d1738c917b373fc6de91
robforner <113190486+robforner@users.noreply.github.com>
2024-04-11 17:11:49 +0800

    modified:   new graph

--- pages/3_⭐️_Popularity_Evolution.py
+++ pages/3_⭐️_Popularity_Evolution.py
@@ -1,45 +1,23 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
+from Homepage import dataset
 
 st.set_page_config(page_title="Artist Popularity Evolution", page_icon="⭐️", layout='wide',)
 
-# Load the processed dataset
-df = pd.read_csv('yearly_popularity.csv')
+playlist_appearances = dataset.groupby('track_artist')['playlist_name'].nunique().reset_index(name='unique_playlists')
 
-# Filter the dataset for the specific artists and ensure chronological sorting
-artists = ['Eminem', 'Ariana Grande', 'Ed Sheeran', 'The Chainsmokers']
-df_filtered = df[df['Artist'].isin(artists)].sort_values('Year')
+# Sorting artists by the number of unique playlist appearances
+playlist_appearances_sorted = playlist_appearances.sort_values(by='unique_playlists', ascending=False)
 
-# Streamlit app layout
-st.markdown("# ⭐️ Artist Popularity Evolution")
-# Convert 'Year' to string for animation_frame in Plotly
-df_filtered['Year'] = df_filtered['Year'].astype(str)
+# Creating the bar chart
+fig = px.bar(playlist_appearances_sorted, x='track_artist', y='unique_playlists',
+             title='Most Playlist Appearances per Artist',
+             labels={'track_artist': 'Artist', 'unique_playlists': 'Number of Unique Playlists'},
+             height=600)
 
-# Create a custom color map for the artists
-color_map = {'Eminem': 'aquamarine', 'Ariana Grande': 'darkmagenta', 'Ed Sheeran': 'goldenrod', 'The Chainsmokers': 'color4'}
+# Improving layout
+fig.update_xaxes(tickangle=-45)
 
-# Create the moving graph using Plotly Express
-fig = px.bar(df_filtered,
-             x="Popularity",
-             y="Artist",
-             color="Artist",
-             animation_frame="Year",
-             orientation='h',
-             range_x=[0, df_filtered['Popularity'].max() + 10],
-             color_discrete_map=color_map)
-
-# Update layout for the custom appearance
-fig.update_layout(
-    showlegend=False,
-    xaxis_title="Yearly certified record sales (+digital singles)",
-    yaxis_title=None,
-    yaxis={'categoryorder':'total ascending'},
-    updatemenus=[dict(type='buttons', showactive=False, y=1, x=0.8, xanchor='left', yanchor='bottom')],
-)
-
-# Update traces for custom bar styling
-fig.update_traces(marker_line_color='rgb(8,48,107)', marker_line_width=1.5, opacity=0.6)
-
-# Display the figure in the Streamlit app
-st.plotly_chart(fig)
+# Display the figure
+fig.show()
